5b99bdba6601a4ec0356ff25cc2abc017f67f577
ali mohebbi <ali.mohebbi@usi.ch>
2022-06-29 17:50:28 +0200

    corr scatter plot and impact analysis

--- correlation_calc/corr_calc.py
+++ correlation_calc/corr_calc.py
@@ -1,23 +1,52 @@
 import glob
 import os
 
+import numpy as np
 import pandas as pd
 import yaml
+from matplotlib import pyplot as plt
+from matplotlib.lines import Line2D
 from pandas import read_csv
 from scipy.stats import pearsonr
+import seaborn as sns
+import ssl
 
+ssl._create_default_https_context = ssl._create_unverified_context
 with open(r'../config.yaml') as file:
     config = yaml.safe_load(file)
 
-if __name__ == '__main__':
-    print(config['test_reuse_full'])
+
+def add_baseline_values(csv_df, file_name):
+    random_config_mrr = {'all': 0.20191013, 'atm': 0.2018414, 'craft': 0.196925}
+    random_config_top1 = {'all': 19.5 / 337, 'atm': 20.5 / 116, 'craft': 19 / 221}
+    csv_df.loc[csv_df['Algorithm'] == 'perfect', 'MRR'] = 1
+    csv_df.loc[csv_df['Algorithm'] == 'perfect', 'Top1'] = 1
+    if '_craft' in file_name:
+        csv_df.loc[csv_df['Algorithm'] == 'random', 'MRR'] = random_config_mrr['craft']
+        csv_df.loc[csv_df['Algorithm'] == 'random', 'Top1'] = random_config_top1['craft']
+    elif '_atm' in file_name:
+        csv_df.loc[csv_df['Algorithm'] == 'random', 'MRR'] = random_config_mrr['atm']
+        csv_df.loc[csv_df['Algorithm'] == 'random', 'Top1'] = random_config_top1['atm']
+    else:
+        csv_df.loc[csv_df['Algorithm'] == 'random', 'MRR'] = random_config_mrr['all']
+        csv_df.loc[csv_df['Algorithm'] == 'random', 'Top1'] = random_config_top1['all']
+    return csv_df
+
+
+def read_results(plot=False):
     full_agg_results = {}
     for path in glob.glob(config['test_reuse_full'] + "/*.csv"):
         csv_df = read_csv(path, encoding='latin-1')
-        csv_df = csv_df[csv_df['MRR'].notna()]
         file_name = os.path.basename(path).split('.')[0]
+        if not plot:
+            csv_df = csv_df[csv_df['MRR'].notna()]
+        else:
+            csv_df = add_baseline_values(csv_df, file_name)
         full_agg_results[file_name] = csv_df
+    return full_agg_results
 
+
+def make_corr_tables(full_agg_results):
     corr_list = []
     for k, v in full_agg_results.items():
         corr_m, p_value_m = pearsonr(list(v['F1 score']), list(v['MRR']))
@@ -25,6 +54,51 @@ if __name__ == '__main__':
         row = {'Name': k, 'Corr MRR': corr_m, 'p_value_m': float(p_value_m), 'Corr Top1': corr_t,
                'p_value_t': float(p_value_t)}
         corr_list.append(row)
-
     corr_df = pd.DataFrame(corr_list)
     corr_df.round(5).to_csv('corr.csv', index=False, float_format='%.5f')
+
+
+def add_legend():
+    custom_lines = [
+        Line2D([0], [0], marker="o", color='gold', markersize="7", lw=0),
+        Line2D([0], [0], marker="o", color='tab:red', markersize="7", lw=0),
+        Line2D([0], [0], marker="o", color='limegreen', markersize="7", lw=0),
+        Line2D([0], [0], marker="o", color='cornflowerblue', markersize="7", lw=0)
+    ]
+    plt.legend(custom_lines, ['Syntactic Configs', 'Random', 'Perfect', 'Other Configs'], loc=2)
+
+
+def get_palette(data):
+    configs = list(data['config'])
+    palette = {}
+    for i in configs:
+        palette[i] = 'cornflowerblue'
+        if 'es' in i or 'js' in i:
+            palette[i] = 'gold'
+    palette['random_NA_NA_NA'] = 'tab:red'
+    palette['perfect_NA_NA_NA'] = 'limegreen'
+    return palette
+
+
+def make_scatter_plot(full_agg_results, axis_x):
+    for k, v in full_agg_results.items():
+        data = v.copy().fillna('NA')
+        data['config'] = data['Algorithm'] + '_' + data['Training'] + '_' + data['Embedding'] + '_' + data[
+            'Descriptors']
+        plt.clf()
+        plt.close()
+        plt.figure(figsize=(10, 5))
+        plt.ylim(0, 1)
+        ax = sns.scatterplot(data=data, x=axis_x, y="F1 score", hue='config', palette=get_palette(data), legend=False, s=50)
+        m, b = np.polyfit(data[axis_x], data["F1 score"], 1)
+        plt.plot(data[axis_x], m * data[axis_x] + b, color='orangered')
+        add_legend()
+        plt.savefig(f'plots/{axis_x}_{k}.pdf', bbox_inches='tight')
+
+
+if __name__ == '__main__':
+    full_agg_results = read_results(False)
+    make_corr_tables(full_agg_results)
+    full_agg_results = read_results(True)
+    make_scatter_plot(full_agg_results, "MRR")
+    make_scatter_plot(full_agg_results, "Top1")
---
+++ impact/impact_analysis.py
@@ -0,0 +1,163 @@
+import os
+import sys
+import warnings
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import yaml
+from matplotlib.lines import Line2D
+from pandas import DataFrame, Series
+import matplotlib.patches as mpatches
+from scipy import stats
+from scipy.stats import ttest_ind
+
+with open(r'../config.yaml') as file:
+    config = yaml.safe_load(file)
+
+
+def plot_boxes_std(data, ax):
+    f = plt.figure()
+    data = pd.concat([pd.Series(v, name=k) for k, v in data.items()], axis=1)
+    data = pd.DataFrame(data)
+    cols = list(data.columns.values)
+    print(cols)
+    warnings.filterwarnings('ignore', r'All-NaN (slice|axis) encountered')
+
+    sorted_index = data.median(skipna=True).sort_values().index
+    data = data[sorted_index]
+    data.copy().describe().round(4).to_csv('tables/impact.csv')
+    palette = get_palette()
+    bplot = sns.boxplot(data=data,
+                        width=0.3,
+                        palette=palette, ax=ax)
+
+    bplot = sns.stripplot(data=data,
+                          jitter=True,
+                          marker='o',
+                          alpha=0.7,
+                          color='black', size=3, ax=ax)
+    bplot.set_xlabel("MRR", fontsize=11, fontweight='bold')
+    bplot.set_ylabel("SD", fontsize=11, fontweight='bold')
+    bplot.tick_params(labelsize=8)
+    bplot.set(xticklabels=[])
+    bplot.set_xticks([])
+
+
+def get_index_random(data: DataFrame):
+    a = set(data.query("algorithm == 'random'").index.tolist())
+    return list(a)[0]
+
+
+def get_values(group, t):
+    desc = group.describe()
+    print(desc.to_string())
+    if t == "std":
+        return group.std()
+    else:
+        return group.var()
+
+
+def impact_analysis_fix_others(df: DataFrame, col: str, t: str):
+    components_names = list(df.columns)
+    components_names.remove(col)
+    components_names.remove("value")
+    group = df.groupby(components_names)["value"]
+    return get_values(group, t)
+
+
+def normality_test(name, data):
+    k2, p = stats.normaltest(data)
+    alpha = 0.05
+    print("**" + str(name) + "**  ")
+    # print(pd.DataFrame(data, columns=['value'])['value'].describe().to_markdown())
+    alpha = 1e-3
+    # print("alpha = " +str(alpha) + "  ")
+    print("p-value = {:g}  ".format(p) + "  ")
+    # p = 3.27207e-11
+    if p < alpha:  # null hypothesis: x comes from a normal distribution
+        print("p <= alpha: reject H0, <span style=\"color:red\">NOT normal distribution</span><br /><br />")
+    else:
+        print("p > alpha: fail to reject H0, <span style=\"color:green\">normal distribution</span>  <br /><br />")
+
+
+def significance_test(name1, name2, data1, data2):
+    print("**" + str(name1) + " <-> " + str(name2) + "**   ")
+    stat, p = ttest_ind(data1, data2)
+    alpha = 0.05
+
+    # print("alpha = 0.05  ")
+    print("p-value = {:g}   ".format(p))
+    # interpret
+    # alpha = 0.05
+    if p < alpha:  # null hypothesis: x comes from a normal distribution
+        print("p <= alpha: reject H0, <span style=\"color:green\">different distributions</span><br /><br />")
+    else:
+        print("p > alpha: fail to reject H0, <span style=\"color:red\">same distributions</span> <br /><br />")
+
+
+def analyse(data: DataFrame, ax):
+    data = remove_unrelated_rows(data)
+
+    components_names = list(data.columns)
+    components_names.remove("value")
+
+    res_fix_others = {}
+    for col in components_names:
+        print("impact analysis for " + col)
+        values: Series = impact_analysis_fix_others(data, col, 'std')
+        print("number cluster " + str(len(values.values)))
+        res_fix_others.update({col: list(values.values)})
+        normality_test("std" + col, res_fix_others.get(col))
+
+    plot_boxes_std(res_fix_others, ax=ax)
+    sinigicant_test(components_names, res_fix_others)
+
+
+def remove_unrelated_rows(data):
+    data = data[~data['word_embedding'].isin(['random'])]
+    data = data[~data['algorithm'].isin(['random'])]
+    data = data[~data['training_set'].isin(['standard'])]
+    data = data[~data['training_set'].isin(['empty'])]
+    return data
+
+
+def sinigicant_test(components_names, res_fix_others):
+    combination = set()  # avoid to tst same synmmetric comb
+    for col1 in components_names:
+        for col2 in components_names:
+            if col1 != col2 and not ((col1 + "<->" + col2) in combination):
+                significance_test(col1, col2, res_fix_others.get(col1), res_fix_others.get(col2))
+                combination.add(col1 + "<->" + col2)
+                combination.add(col2 + "<->" + col1)
+
+
+def get_palette():
+    return {'training_set': 'tab:blue', 'word_embedding': 'tab:green',
+            'algorithm': 'tab:orange', 'descriptors': 'orangered'}
+
+
+def add_legend(fig):
+    palette = get_palette()
+    custom_lines = [
+        Line2D([0], [0], color=j, markersize="7", lw=8) for j in palette.values()
+    ]
+    fig.legend(custom_lines, palette.keys(), title='Component type', ncol=4, loc='lower center'
+               , bbox_to_anchor=(0.5, -0.1), fontsize=15, title_fontsize=15)
+
+
+def main_plot():
+    data = pd.read_csv(config['test_reuse_plot'] + '/craftdroid_all_oracle_forplot.csv')
+
+    fig, axes = plt.subplots(1, 2, figsize=(20, 5), sharey=True)
+    analyse(data, axes[0])
+    data = pd.read_csv(config['test_reuse_plot'] + '/atm_atm_oracle_passfree_forplot.csv')
+    analyse(data, axes[1])
+    axes[0].set_title('CraftDroid', fontsize=15, fontweight='bold')
+    axes[1].set_title('ATM', fontsize=15, fontweight='bold')
+    add_legend(fig)
+    fig.savefig('plots/impact.pdf', bbox_inches='tight')
+    plt.show()
+
+
+if __name__ == "__main__":
+    main_plot()
